8540d4c7aa5e755a1e91b4fbf56ccc26a57b5f00
Viktoriia Chernyshuk <vickierose.ch@gmail.com>
2019-10-23 14:30:39 +0300

    clean up

--- src/components/App.js
+++ src/components/App.js
@@ -1,44 +1,8 @@
 import React from 'react'
-import { BrowserRouter as Router } from "react-router-dom"
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
-import { I18nProvider } from '@lingui/react'
-
-import catalogEn from '../i18n/en/messages'
-import catalogRu from '../i18n/ru/messages'
-import catalogUk from '../i18n/uk/messages'
-
-import Routing from './Routing'
-import { Header } from './common'
-
-const catalogs = {
-  en: catalogEn,
-  ru: catalogRu,
-  uk: catalogUk
-};
-
-const theme = createMuiTheme({
-  typography: {
-    useNextVariants: true,
-  },
-  palette: {
-    primary: {
-      main: '#ffcad4'
-    }
-  }
-});
 
 const App = () => {
   return (
-    <MuiThemeProvider theme={theme}>
-      <I18nProvider language="ru" catalogs={catalogs}>
-        <Router>
-          <>
-            <Header />
-            <Routing />
-          </>
-        </Router>
-      </I18nProvider>
-    </MuiThemeProvider>
+    <div>here will be blitz</div>
   )
 }
 
--- src/components/common/index.js
+++
@@ -1,7 +0,0 @@
-import Header from './Header'
-import Logo from './Logo'
-
-export {
-  Header,
-  Logo
-}
